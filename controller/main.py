abcc72a13e9969f237a9f6903ddc9ca0630fae7c
Chris Wong <chriswwl95@gmail.com>
2023-08-05 15:57:16 +1000

    hourly data

--- controller/main.py
+++ controller/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
+import open_weather as ow
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "root of Open Weather!"}
+    return {"message": "root of raincheck"}
 
 @app.get("/?city={city}")
 def getCity(city : str):
     return {"city": city}
 
-@app.get("/weather/")
-def getWeather(lon : float = 12, lat : float = 10):
-    return {"lon": lon, "lat": lat}
+@app.get("/weather")
+async def getWeather(lat : float = 12, lon : float = 10):
+    weather = ow.getWeatherNow(lon, lat)
+    return weather
+
+@app.get("/weather/hourly")
+async def getWeatherHourly(lat : float = 12, lon : float = 10):
+    weather = ow.getWeatherHourly(lon, lat)
+    return weather
+
--- controller/open_weather.py
+++ controller/open_weather.py
@@ -1,7 +1,78 @@
-import requests as req
+import requests as req, os
+from dotenv import load_dotenv
 
+load_dotenv()
 base_url = "https://api.openweathermap.org/data/2.5"
+ow_key = os.getenv("OW_KEY")
 
-class OpenWeather():
-    def __init__(self, city):
-        self.city = city
+def getWeatherNow(lat, lon, metric=True):
+    path = "/weather"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": ow_key,
+        "units": "metric" if metric else "imperial"
+    }
+    r = req.get(base_url + path, params=params)
+    data = r.json()
+    weather = {
+        "country" : data["sys"]["country"],
+        "city": data["name"],
+        "temp": data["main"]["temp"],
+        "max" : data["main"]["temp_max"],
+        "min" : data["main"]["temp_min"],
+        "wind_speed": data["wind"]["speed"],
+        "humidity": data["main"]["humidity"],
+        "feels_like": data["main"]["feels_like"],
+        "description": data["weather"][0]["main"],
+        "icon" : data["weather"][0]["icon"],
+    }
+    return weather
+
+def getWeatherHourly(lat, lon, metric=True):
+    path = "/forecast"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": ow_key,
+        "units": "metric" if metric else "imperial"
+    }
+    r = req.get(base_url + path, params=params)
+    data = r.json()
+    weather_list = []
+    # TODO:
+    # Find a way to show only the future from date.now
+    for i in data["list"]:
+        meta_data ={
+            "datetime" : i["dt_txt"],
+            "temp" : i["main"]["temp"],
+            "max" : i["main"]["temp_max"],
+            "min" : i["main"]["temp_min"],
+            "wind_speed": i["wind"]["speed"],
+            "humidity": i["main"]["humidity"],
+            "feels_like": i["main"]["feels_like"],
+            "description": i["weather"][0]["main"],
+            "icon" : i["weather"][0]["icon"]
+        }
+        weather_list.append(meta_data)
+
+    weather = {
+        "country" : data["city"]["country"],
+        "city": data["city"]["name"],
+        "list" : weather_list
+    }
+    return weather
+
+def getWeatherDaily(lat, lon, metric=True):
+    path = "/forecast/daily"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "cnt" : 10,
+        "appid": ow_key,
+        "units": "metric" if metric else "imperial"
+    }
+    r = req.get(base_url + path, params=params)
+    data = r.json()
+    
+    return 0
